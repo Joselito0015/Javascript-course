bd322a782255ee8c6d28bd90d08b838dbb7de46e
Joselito0015 <luis.blas@pucp.edu.pe>
2021-10-28 22:40:54 -0500

    First Commit

---
+++ Js/main.js
@@ -0,0 +1,147 @@
+/* Variables Globales */
+var alertaUsuario=''
+var miembrosFamilia
+var list =[]
+var  aux
+
+/* Clases */
+class user{
+    constructor (number)
+    {   this.number=number
+        this.sexo=prompt('Usuario ' + this.number + ':\nCuál es su sexo?\nhombre: H \nmujer: M')
+        this.nombre= prompt('Usuario ' + this.number + ':\nCual es tu nombre? :)')
+        this.peso=prompt('Usuario ' + this.number + ':\nPeso en kilogramos: \nejmp: 58')
+        this.talla=prompt('Usuario ' + this.number + ':\nCual es tu talla en metros?\nejemp:1.80')
+        this.determinarPuntosCriticos()
+    }
+    
+    /* Métodos */
+    calcularImc(){
+        this.imc= this.peso/Math.pow(this.talla,2)
+    }
+
+    determinarPuntosCriticos(){ 
+        if (this.sexo=="M"){
+            this.bajopeso = 20
+            this.normal = 23.9
+            this.obesidadleve = 28.9
+            this.obesidadsevera = 37
+            this.obesidadmuysevera = 37
+        }
+        else if (this.sexo=="H"){
+            this.bajopeso = 20
+            this.normal = 24.9
+            this.obesidadleve = 29.9
+            this.obesidadsevera = 40
+            this.obesidadmuysevera = 40
+        }
+    }
+
+    interpretacionIMC(){
+
+        if(this.imc<this.bajopeso){
+            alert('Usuario ' + this.number + ':\nLa interpretación de tu imc: Te encuentras en peso muy bajo para tu sexo ' + this.sexo)
+        }
+        else if(this.imc>=this.bajopeso && this.imc<this.normal){
+            alert('Usuario ' + this.number + ':\nLa interpretación de tu imc: Te encuentras en peso normal para tu sexo ' + this.sexo)
+        }
+        else if(this.imc>=this.normal && this.imc<this.obesidadleve){
+            alert('Usuario ' + this.number + ':\nLa interpretación de tu imc: Te encuentras en obesidad leve para tu sexo ' + this.sexo)
+        }
+        else if(this.imc>=this.obesidadleve && this.imc<this.obesidadsevera){
+            alert('Usuario ' + this.number + ':\nLa interpretación de tu imc: Te encuentras en obesidad severa para tu sexo '+ this.sexo)
+        }
+        else if(this.imc>=this.obesidadmuysevera){
+            alert('Usuario ' + this.number + ':\nLa interpretación de tu imc: Te encuentras en obesidad muy severa para tu sexo '+ this.sexo)
+        }
+    }
+
+    saludo(){
+        alert('Usuario ' + this.number + ':\nHola! Vamos a pedirte unos datos para calcular tu indice de masa corporal')
+    }
+
+
+    GenerarAlerta(){
+        aux='Usuario ' + this.number + ':\n'+ this.nombre+ ", tu índice de masa corporal es: " + this.imc.toFixed(2)
+        alertaUsuario=alertaUsuario + '\n' + aux + '\n'
+        alert(alertaUsuario)
+        console.log(this.nombre + " : " + this.imc.toFixed(2))
+    }    
+
+}
+
+/* Funciones */
+function datosDeFamilia(){
+    miembrosFamilia = prompt("Cuidamos a tu familia ¿Cuantos miembros son en tu familia?")
+}
+
+/* Desafio funciones relacionales */
+function ConsultarPlanNutriconal(){
+    respuesta=prompt("Desea comprar uno de nuestros planes nutricionales\nSí =1\nNo =0")
+    return respuesta
+}
+
+function ConsultarOpcion(){
+    opcion=prompt("Paquete Premium = 1\nPaquete Platinum = 2\nPaquete Lite =3")
+    return opcion
+}
+
+function calcularPlanNutricional(respuesta,opcion){
+
+    if (respuesta== "1"){
+        alert("No te fallaremos, haremos todo lo posible por ayudar a tu salud :D")
+        
+        if (opcion=="1"){
+            subtotal= miembrosFamilia*450
+            total= subtotal * 0.92 //taxes
+            total
+        }
+        else if (opcion=="2"){
+            subtotal= miembrosFamilia*150
+            total= subtotal * 0.92 //taxes
+            total
+        }
+        else if (opcion=="3"){
+            subtotal= miembrosFamilia*100
+            total= subtotal * 0.92 //taxes
+            total
+        }
+
+        alert("El paquete tiene un costo total de " + total +" USD para proteger a toda tu familia")
+    }
+    else {
+        alert("Esperamos verte pronto!")
+    }
+}
+
+
+/* ---- MAIN ----*/
+datosDeFamilia()
+for (var i=1; i<=miembrosFamilia;i++){
+    //Se cabió el diccionario por una lista de objetos --> Desafío
+    list[i] =  new user(i);
+    list[i].calcularImc()
+    list[i].GenerarAlerta()
+    list[i].interpretacionIMC()    
+    console.log(list[i])
+}
+
+//Ordenamos el array personas por medio de su IMC calculado
+
+console.log(list.length)
+list.sort((a,b) => a.imc-b.imc);
+
+
+//Mostramos  todos los usuarios ordenados  en la consola desde la persona más saludable a la menos saludable ---> Desafío complementario
+for (var i=0; i<=miembrosFamilia-1;i++){ 
+    console.log(list[i])
+}
+
+if (ConsultarPlanNutriconal!= "0"){
+    calcularPlanNutricional(ConsultarPlanNutriconal(),ConsultarOpcion())
+} 
+
+
+
+
+    
